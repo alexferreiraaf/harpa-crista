623bd27cb264679b30b9d5a44d777c25c8d9a539
Alex Ferreira <alexferreira.AF411@gmail.com>
2025-09-03 04:47:16 +0000

    Eu quero saber como faço para alterar o ícone da logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -43,8 +43,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M7 18a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4h1a3 3 0 0 1 3 3v10a4 4 0 0 1-4 4Z" />
-      <path d="M12 18a4 4 0 0 0 4-4V6a4 4 0 0 0-4-4h-1a3 3 0 0 0-3 3v10a4 4 0 0 0 4 4Z" />
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
     </svg>
   );
 }
